f8c7ed582559e4029b1ecbc3b76904f4809d58d3
Mykola <jacktyler2391@gmail.com>
2024-11-25 14:30:24 +0200

    Configure project settings. Create & register app models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from app.models import User, Task, Tag
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + (
+        "first_name",
+        "last_name",
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "content",
+        "created",
+        "deadline",
+        "status",
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True)
+    status = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", blank=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Tag(models.Model):
+    name = models.TextField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
